a5e81bd42cb7d25d542c250978b2e2881d6dd0ac
Stefan Kupresak <stefan_vg@hotmail.com>
2023-08-02 01:23:55 +0200

    update path to certs

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,8 @@ async fn main() {
     dotenv().expect(".env file not found!");
     tracing_subscriber::fmt().init();
 
-    let cert = include_bytes!("../certs/cert.pem").to_vec();
-    let key = include_bytes!("../certs/key.pem").to_vec();
+    let cert = include_bytes!("../../fullchain.pem").to_vec();
+    let key = include_bytes!("../../certs/privkey.pem").to_vec();
     let config = RustlsConfig::new(Keycert::new().cert(cert.as_slice()).key(key.as_slice()));
     let listener = TcpListener::new(("127.0.0.1", 443)).rustls(config.clone());
 
